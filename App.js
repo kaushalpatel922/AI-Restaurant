a9c4ee8c97682103cf839e0309b601b4977641b1
Kaushal Patel <kaushalpatel922@gmail.com>
2025-03-27 18:10:53 -0400

    Creating app.js component

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+function App() {
+    return (
+        <div className="App">
+            <h1>AI Nail Recommendation</h1>
+        </div>
+    );
+}
+
+export default App;
