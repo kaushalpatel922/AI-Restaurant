d0a3fe67e4ca10bc68148e8b53a03f939affc292
Kaushal Patel <kaushalpatel922@gmail.com>
2025-03-28 15:36:13 -0400

    Adding and importing UploadImage component

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
 import React from "react";
-
+import UploadImage from "./components/UploadImage";
 
 function App() {
-    return (
-        <div className="App">
-            <h1>AI Nail Recommendation</h1>
-        </div>
-    );
+  return (
+    <div className="App">
+      <h1>AI Nail Recommendation</h1>
+      <UploadImage />
+    </div>
+  );
 }
 
 export default App;
