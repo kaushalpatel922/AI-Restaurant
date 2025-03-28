7de192e4b3e90b2601d9405f882dcf72151800ed
Kaushal Patel <kaushalpatel922@gmail.com>
2025-03-28 15:53:22 -0400

    adding request and error handling

--- UploadImage.js
+++ UploadImage.js
@@ -8,10 +8,27 @@ const uploadImage = () => {
     setImage(e.target.files[0]);
   };
 
+  const handleUpload = async () => {
+    if(!image) {
+      alert("Please upload an image!")
+      return;
+    }
+
+    const formData = new FormData();
+    formData.append("file", image);
+
+    try {
+      const response = await axios.post("http://127.0.0.1:5000/analyze", formData);
+    } catch(error) {
+      alert("Error processing image!")
+    }
+  }
+
   return (
     <div className="upload-container">
       <h1>Upload Image here..</h1>
       <input type="file" onChange={handleImageChange} />
+      <button onClick={handleUpload}>Analyze</button>
     </div>
   );
 };
