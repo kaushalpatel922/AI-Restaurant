44c00db760132a5902c7cb7415d5376796668385
Kaushal Patel <kaushalpatel922@gmail.com>
2025-03-31 12:29:30 -0400

    analyze function

--- Backend/app.py
+++ Backend/app.py
@@ -1,6 +1,15 @@
 from flask import Flask, request
+import cv2
+import numpy as np
+from PIL import Image
+import io
 
 app = Flask(__name__)
+
+def analze_nail():
+    file = request.files["file"]
+    image = Image.open(io.BytesIO(file.read()))
+    image = np.array(image)
 
 if __name__ = "__main__":
     app.run(debug=True)
