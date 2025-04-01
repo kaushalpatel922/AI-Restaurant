4312b6a2af7429f84e9d6bdf4f12414968f03d22
Kaushal Patel <kaushalpatel922@gmail.com>
2025-04-01 15:09:17 -0400

    adding detect hand

--- Backend/app.py
+++ Backend/app.py
@@ -6,6 +6,20 @@ import io
 
 app = Flask(__name__)
 
+NAIL_COLOR_MAP = {
+    "fair": ["Pastel Pink", "Nude", "Light Blue"],
+    "medium": ["Deep Red", "Burgundy", "Rose Gold"],
+    "dark": ["Metalic Gold", "Dark Green", "Vibrant Purple"]
+}
+
+def detect_hand(image):
+    image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    results = hands.process(image_rgb)
+
+    if results.multi_hand_landmarks:
+        return True, image_rgb
+    return False, None
+
 def analze_nail():
     file = request.files["file"]
     image = Image.open(io.BytesIO(file.read()))
